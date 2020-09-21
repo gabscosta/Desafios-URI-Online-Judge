0372b46e2ba4f5df251574fda8bbac8b596c9b72
gabscosta <69794293+gabscosta@users.noreply.github.com>
2020-09-21 13:45:59 -0300

    dia1
    dia1

---
+++ uri1003.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+n1 = int(input())
+n2 = int(input())
+soma = n1 + n2
+print("SOMA = %d" %soma)
---
+++ uri1004.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+n1 = int(input())
+n2 = int(input())
+produto = n1 * n2
+print("PROD = %d" %produto)
